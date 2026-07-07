f0b1230287bcc5cca982806f4ce8f214096cd9f2
Vic <vicxd@pm.me>
2026-07-07 14:38:06 +0200

    create_tos_disk implementation & bug fix

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::path::{self, Path};
 use std::process::{Command, Stdio};
 
-
+//TODO support a custom assets dir via a flag
 const TOS_DISK_PATH: &str = "./assets/templeOS-disk.qcow2";
 const TOS_IMAGE_LOCAL_PATH: &str = "./assets/TempleOS.ISO";
 const TOS_IMAGE_REMOTE_PATH: &str = "https://templeos.org/Downloads/TempleOS.ISO";
@@ -14,15 +14,34 @@ fn main() {
     check_dependencies();
 
     if !tos_disk.exists() {
-        create_tos_disk();
+        create_tos_disk(&tos_disk);
     }
 
 
     println!("Hello, world!");
 }
 
-fn create_tos_disk() {
-    todo!()
+fn create_tos_disk(path: &Path) {
+    let disk_size = "2G";
+
+    let path = path.to_str().unwrap();
+    println!("INFO: Storage disk not found in {TOS_DISK_PATH}, attempting to create a 2G one via `qemu-img`\n");
+    let status = Command::new("qemu-img")
+                    .arg("create")
+                    .args(["-f", "qcow2"])
+                    .arg(path)
+                    .arg(disk_size)
+                    .status()
+                    .unwrap();
+
+    println!();
+    match status.success() {
+        true => println!("INFO: Successfully created the disk"),
+        false => {
+                println!("ERROR: failed to create disk. aborting");
+                std::process::exit(1);
+        }
+    }
 }
 
 fn start_tos_installation() {
@@ -34,9 +53,17 @@ fn start_tos_installation() {
 }
 
 fn download_tos_image(path: &Path) {
+    let path = path.to_str().unwrap();
+    println!("INFO: TempleOS ISO not found in \n{TOS_IMAGE_LOCAL_PATH}\n, attempting to download from {TOS_IMAGE_REMOTE_PATH}");
     let res = Command::new("wget")
         .arg(TOS_IMAGE_REMOTE_PATH)
-        .args(["-o", path.to_str().unwrap()]);
+        .args(["-o", path])
+        .status()
+        .unwrap();
+    if !res.success() {
+        println!("ERROR: failed to download the TempleOS image");
+        std::process::exit(1);
+    }
 }
 
 const REQUIRED_CMDS: &[&str] = &["qemu-system-x86_64", "qemu-img", "wget"];
