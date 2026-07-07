0f13a531697998a8250d14f34f81e025598a0e2d
Vic <vicxd@pm.me>
2026-07-07 13:05:34 +0200

    something somehow

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::fs;
 use std::path::Path;
+use std::process::Command;
 
 const TOS_DISK_PATH: &str = "./assets/templeOS-disk.qcow2";
+const TOS_IMAGE_LOCAL_PATH: &str = "./assets/TempleOS.ISO";
+const TOS_IMAGE_REMOTE_PATH: &str = "https://templeos.org/Downloads/TempleOS.ISO";
 
 fn main() {
     let tos_disk = Path::new(TOS_DISK_PATH);
@@ -15,4 +18,18 @@ fn main() {
 
 fn create_tos_disk() {
     todo!()
+}
+
+fn start_tos_installation() {
+    let tos_image_local = Path::new(TOS_IMAGE_LOCAL_PATH);
+    let tos_image_local = tos_image_local.absolute();
+    if !tos_image_local.exists() {
+        download_tos_image(tos_image_local);
+    }
+}
+
+fn download_tos_image(path: Path) {
+    let res = Command::new("wget")
+        .arg(TOS_IMAGE_REMOTE_PATH)
+        .args(["-o", ])
 }
