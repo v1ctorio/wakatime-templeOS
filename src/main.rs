4b2129cb88bfdab144d7e1b3f8b2fb5ed24508d1
Vic <vicxd@pm.me>
2026-07-07 11:55:56 +0200

    something

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use std::path::Path;
+
+const TOS_DISK_PATH: &str = "./assets/templeOS-disk.qcow2";
+
 fn main() {
+    let tos_disk = Path::new(TOS_DISK_PATH);
+
+    if !tos_disk.exists() {
+        create_tos_disk();
+    }
+
     println!("Hello, world!");
+}
+
+fn create_tos_disk() {
+    todo!()
 }
